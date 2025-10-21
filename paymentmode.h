77921545bcc8e6be0210f8ad24508b2e71f06bf1
Rohan16298 <urk3hyd@bosch.com>
2025-10-21 10:08:22 +0530

    Add PaymentMode enum for payment options

---
+++ paymentmode.h
@@ -0,0 +1,11 @@
+#ifndef DC_PAYMENTMODE_HPP_INCLUDED
+#define DC_PAYMENTMODE_HPP_INCLUDED
+
+enum class PaymentMode {
+    PayPal,
+    GooglePay,
+    CreditCard,
+    Unknown
+};
+
+#endif // DC_PAYMENTMODE_HPP_INCLUDED
